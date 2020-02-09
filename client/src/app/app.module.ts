64a3894559be4206ac825b179b88eeba8e3328dc
Hristo Manov <45336093+hmanov@users.noreply.github.com>
2020-02-09 12:35:26 +0200

    Added Home, Login, Register pages

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
---
+++ client/src/app/layout/navbar/navbar.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import {} from 'protractor';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent implements OnInit {
+  constructor() {}
+  @Input() collapse: boolean = false;
+  ngOnInit() {}
+
+  toggleNavbarMenu() {
+    this.collapse = !this.collapse;
+    console.log(this.collapse);
+  }
+}
